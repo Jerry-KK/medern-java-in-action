2b8460ad6a0edbebc805e52f3bf467940aff8745
lethekk <15868308739@163.com>
2021-07-16 00:06:03 +0800

    # chapter4 流简介

---
+++ src/main/java/chapter4/useStream.java
@@ -0,0 +1,27 @@
+package chapter4;
+
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * @author lethe
+ * @date 2021/7/15 23:24
+ */
+public class useStream {
+    public static void main(String[] args) {
+        List<String> threeHeightCaloricDishNames = Constant.menu
+                //从menu（菜肴列表）获得流
+                .stream()
+                //建立操作流水线：首先选出高热量的菜肴
+                .filter(dish -> dish.getCalories() > 300)
+                //获取菜名
+                .map(Dish::getName)
+                //只选择头三个
+                .limit(3)
+                //将结果保存在另一个List
+                .collect(toList());
+        System.out.println(threeHeightCaloricDishNames);
+    }
+}
